41bf8cb4a5e9b27ed1eb6c05766ebef83c9c8239
Mirr218 <st.pt01@mail.ru>
2026-04-17 18:47:46 +0600

    feat(schemas): Схема user для валидации

---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+
+class UserResponse(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserInfo(UserResponse):
+    pass
