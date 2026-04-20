6a2c8474be1a4ac685037f9c76597d1e99f94044
Mirr218 <st.pt01@mail.ru>
2026-04-20 22:50:48 +0600

    feat: exceptions на отдельном слое чтобы не использовть напрямую в бизнес логике

---
+++ app/exceptions.py
@@ -0,0 +1,17 @@
+class AppException(Exception):
+    pass
+
+class UserAlreadyExistsError(AppException):
+    pass
+
+class InvalidCredentialsError(AppException):
+    pass
+
+class FormNotFoundError(AppException):
+    pass
+
+class AccessDeniedError(AppException):
+    pass
+
+class InvalidAnswersError(AppException):
+    pass
